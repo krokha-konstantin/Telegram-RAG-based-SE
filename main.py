7578fe05a6fac3d87373041c306a47811e34d5dd
krokha-konstantin <164772432+krokha-konstantin@users.noreply.github.com>
2026-03-28 18:57:33 +0300

    init fix

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from src.parser.telegram_parser import parse
 from src.db.database import init_db
 from src.bot.telegram_bot import start_bot
 
-# conn = init_db()
-# asyncio.run(parse(conn))
-# conn.commit()
-# conn.close()
+conn = init_db()
+asyncio.run(parse(conn))
+conn.commit()
+conn.close()
 
 start_bot()
